2629f6d96efeb662930a8d6e3d0839517bbc5b2b
PascaleBogdan <pascale.bogdan@yahoo.com>
2024-03-12 12:44:31 +0200

    Your commit message

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '096a0677efmsh9e0a0dd4694b346p194717jsn09bdbc3deaa9'
+  };
+  
